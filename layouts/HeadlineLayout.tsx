109bf9294e908e3be1f82caabf05eac50e02d41d
Sean McGregor <gitcommit@seanbmcgregor.com>
2026-04-17 21:48:14 -0700

    clean up content

--- layouts/HeadlineLayout.tsx
+++ layouts/HeadlineLayout.tsx
@@ -14,9 +14,6 @@ export default function HeadlineLayout({ children, content }: Props) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
           <div className="prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-3">
-            <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-              Welcome
-            </h1>
             <div>{status?.raw}</div>
             {children}
           </div>
