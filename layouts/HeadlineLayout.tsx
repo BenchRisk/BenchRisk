b488a8a7917bf14dd522a83d9e90e8984cfeea8b
Sean McGregor <sean.mcgregor@averi.org>
2026-04-18 05:03:31 +0000

    only render the status div when it has content

--- layouts/HeadlineLayout.tsx
+++ layouts/HeadlineLayout.tsx
@@ -14,7 +14,7 @@ export default function HeadlineLayout({ children, content }: Props) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
           <div className="prose max-w-none pb-8 pt-0 dark:prose-invert xl:col-span-3">
-            <div>{status?.raw}</div>
+            {status?.raw && <div>{status.raw}</div>}
             {children}
           </div>
         </div>
